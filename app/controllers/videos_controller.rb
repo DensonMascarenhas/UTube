ad853666a312c67ee55afcb6989112d4ef37b2da
Mascarenhas <dmascare@Densons-MBP.paychex.com>
2022-12-16 15:07:40 +0530

    First Commit

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,20 +1,30 @@
 class VideosController < ApplicationController
+    before_action :authenticate_user!, except:[:index, :show]
 
     def index 
         @videos=Video.all
     end
 
-
+    # method creates new object
     def new
-        @videos=Video.new
+        @video=current_user.videos.build
     end
 
     def edit
 
     end
 
+    # method saves the record inside model.
     def create 
-        # @videos=Video.new(get_params)
+        @video=current_user.videos.build(get_params)
+        if @video.save
+            flash[:notice]="Saved Succesfully."
+            redirect_to root_path
+        else
+            render 'new', status: :unprocessable_entity
+
+        end
+
     end
 
     def show
@@ -25,7 +35,7 @@ class VideosController < ApplicationController
     private
 
     def get_params
-        params.require(:videos).permit(:title, :description, :link)
+        params.require(:video).permit(:title, :description, :link)
     end
 
 
